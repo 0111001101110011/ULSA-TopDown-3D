8d42b3c1419fbbbc78bdaaaaf414a1f5e94c8397
0111001101110011 <gapm55@gmail.com>
2020-04-24 20:59:14 -0700

    1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 10f)]
+    float moveSpeed = 3.5f;
+
+    [SerializeField, Range(0f, 10f)]
+    float minDistance = 5f;
+
+    NavMeshAgent navMeshAgent;
+
+    void Update()
+    {
+        if(AttackRange)
+        {
+            //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            navMeshAgent.destination = GameManager.instance.Player.transform.position;
+            transform.LookAt(GameManager.instance.Player.transform);
+        }
+    }
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    bool AttackRange
+    {
+        get => Vector3.Distance(this.transform.position, GameManager.instance.Player.transform.position) <= minDistance;
+    }
+}
---
+++ Assets/Scripts/Inventory/Consumable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : Item
+{
+    public virtual void Drink()
+    {
+        // DestroyImmediate(this, true);
+        Debug.Log(name + " Consumed");
+    }
+}
